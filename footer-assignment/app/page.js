85547fc2d126c5a7645fd63b814493a2261cf7dd
AbuBakar Chohan <132102392+Abubakar-09@users.noreply.github.com>
2024-10-09 22:38:24 +0500

    Add files via upload

---
+++ footer-assignment/app/page.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Foooter";
+
+export default function Home() {
+  return (
+    <>
+    <Navbar />
+    <div>main page</div>
+    <Footer />
+    </>
+  );
+}
