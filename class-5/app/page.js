999091cdaff12484ad5d0c35bf2a1bc87e48299e
AbuBakar Chohan <132102392+Abubakar-09@users.noreply.github.com>
2024-10-23 16:59:50 +0500

    Add files via upload

---
+++ class-5/app/page.js
@@ -0,0 +1,14 @@
+// import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+    <div className="bg-red-500 h-screen flex w-screen gap-2 items-center px-2">
+      <div className="bg-green-500 w-full h-40">a</div>
+      <div className="bg-green-500 w-full h-40">s</div>
+      <div className="bg-green-500 w-full h-40">d</div>
+    </div>
+    </>
+  );
+}
