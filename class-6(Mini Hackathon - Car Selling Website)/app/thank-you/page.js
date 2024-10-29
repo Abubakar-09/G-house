1ef3d5d1c808484c0987c6fa0692e45b55c30e52
AbuBakar Chohan <132102392+Abubakar-09@users.noreply.github.com>
2024-10-29 16:17:53 +0500

    Add files via upload

---
+++ class-6(Mini Hackathon - Car Selling Website)/app/thank-you/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div className='w-[80vw] m-auto flex flex-col justify-center items-center h-screen gap-4'>
+        <h1 className='text-6xl text-blue-500 font-bold'>Thank You For Shopping!!!</h1>
+        <p className='text-sm'>Your Order Will be Deliver in 3 Business Days</p>
+    </div>
+    </>
+  )
+}
+
+export default page
