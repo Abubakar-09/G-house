6b9b349e7aca18de61c81bcffc0630ff840946f6
AbuBakar Chohan <132102392+Abubakar-09@users.noreply.github.com>
2024-10-29 16:17:15 +0500

    Add files via upload

---
+++ class-6(Mini Hackathon - Car Selling Website)/app/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-black  text-white flex w-screen justify-center'>Created By AbuBakar Chohan</div>
+  )
+}
+
+export default Footer
