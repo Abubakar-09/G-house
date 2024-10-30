21d8f5f9a1016785b75d8f305c36fd8db1e03a55
AbuBakar Chohan <132102392+Abubakar-09@users.noreply.github.com>
2024-10-30 13:03:41 +0500

    Delete class-6(Mini Hackathon - Car Selling Website) directory

--- class-6(Mini Hackathon - Car Selling Website)/app/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div className='bg-black  text-white flex w-screen justify-center'>Created By AbuBakar Chohan</div>
-  )
-}
-
-export default Footer
